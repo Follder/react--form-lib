b6ad6b468256ae925ce0bb2d47874d9861901c1b
Dmytro Koriahin <Dkoryahin@gmail.com>
2024-09-27 16:17:13 +0300

    final project

--- src/components/inputGroup/InputGroup.tsx
+++ src/components/inputGroup/InputGroup.tsx
@@ -5,19 +5,11 @@ import { Size } from "../../types/types";
 import { Required } from "../../types/types";
 import { LeftIcon } from "../../types/types";
 import { RightIcon } from "../../types/types";
-<<<<<<< HEAD
-import { generateClassname } from "../../utils/generateClassname";
-import { getClassnameRow } from "../../utils/getClassnameRow";
-=======
->>>>>>> sb
 import InputLabel from "../InputLabel/InputLabel";
 import InputAnnotation from "../InputAnnotation/InputAnnotation";
 import InputText from "../InputText/InputText";
 import { getRequiredOption } from "../../utils/getRequiredOption";
-<<<<<<< HEAD
-=======
 import { getFormElementClassNames } from "../../utils/getFormElementClassNames";
->>>>>>> sb
 
 export interface InputGroupProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
   labelPosition?: LabelPosition;
@@ -58,79 +50,6 @@ const InputGroup: React.FC<InputGroupProps> = ({
   ...nativeInputProps
 }) => {
   const requiredOption = getRequiredOption(required, requiredText, helperText);
-<<<<<<< HEAD
-  const getSizeClassname = () => `form-element_${size}`;
-  const formElementClassnames = [];
-  const inputElementClassnames = [];
-
-  formElementClassnames.push(
-    generateClassname(
-      labelPosition,
-      "top",
-      "form-element_top",
-      "form-element_side"
-    )
-  );
-  formElementClassnames.push(
-    generateClassname(required, true, "form-element_attention")
-  );
-  formElementClassnames.push(
-    generateClassname(required, true, "form-element_attentionText")
-  );
-  formElementClassnames.push(
-    generateClassname(isQuiet, true, "form-element_quiet")
-  );
-  formElementClassnames.push(
-    generateClassname(isValid, false, "form-element_error")
-  );
-  formElementClassnames.push(
-    generateClassname(
-      leftIcon,
-      "search",
-      "form-element_icon-left form-element_icon-left-search"
-    )
-  );
-  formElementClassnames.push(
-    generateClassname(
-      leftIcon,
-      "user",
-      "form-element_icon-left form-element_icon-left-user"
-    )
-  );
-  formElementClassnames.push(
-    generateClassname(
-      leftIcon,
-      "password",
-      "form-element_icon-left form-element_icon-left-password"
-    )
-  );
-  formElementClassnames.push(
-    generateClassname(
-      leftIcon,
-      "email",
-      "form-element_icon-left form-element_icon-left-mail"
-    )
-  );
-  formElementClassnames.push(
-    generateClassname(rightIcon, "help", "form-element_icon-right")
-  );
-  formElementClassnames.push(
-    generateClassname(isCmndIcon, true, "form-element_icon-right-offset")
-  );
-  formElementClassnames.push(getSizeClassname());
-  formElementClassnames.push(
-    generateClassname(isDisabled, true, "form-element_disabled")
-  );
-  inputElementClassnames.push(
-    generateClassname(isCmndIcon, true, "form-element_icon-cmnd")
-  );
-
-  const formElement = getClassnameRow("form-element", formElementClassnames);
-  const inputElement = getClassnameRow(
-    "form-element__input",
-    inputElementClassnames
-  );
-=======
 
   const { formElement, inputElement } = getFormElementClassNames({
     labelPosition,
@@ -143,7 +62,6 @@ const InputGroup: React.FC<InputGroupProps> = ({
     isDisabled,
     size
   })
->>>>>>> sb
 
   return (
     <div className={formElement}>
@@ -155,16 +73,8 @@ const InputGroup: React.FC<InputGroupProps> = ({
         />
         <div className={inputElement}>
           <InputText
-<<<<<<< HEAD
-            nativeInputProps={nativeInputProps}
-            placeholder={placeholder}
-            isDisabled={isDisabled}
-            required={required}
-            requiredOption={requiredOption}
-=======
             {...nativeInputProps}
             placeholder={placeholder}
->>>>>>> sb
             customClassname={customClassname}
           />
         </div>
@@ -178,8 +88,4 @@ const InputGroup: React.FC<InputGroupProps> = ({
   );
 };
 
-<<<<<<< HEAD
 export default InputGroup;
-=======
-export default InputGroup;
->>>>>>> sb
